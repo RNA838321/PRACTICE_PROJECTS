6aafabaae71590b4ae5a4d325367feb97df02ca2
RNA838321 <rodneyakong@gmail.com>
2022-11-18 13:26:32 +0000

    numbers 0-10

---
+++ alphabet_manipulation/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+//Title:Write a program that prints all single digit numbers of base 10 starting from 0, followed by a new line.
+//Author: Akong Rodney
+//DAte:11/18/2022
+//Time:2:19pm
+
+int main()
+{
+	int num = 0;
+
+	for (num = 0; num < 10; num++)
+	{
+		printf("%d\n",num);
+	}
+}	
