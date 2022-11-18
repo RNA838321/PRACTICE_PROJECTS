742e6d05e1fceb6b5c6a06aeedad734932a2c1f7
RNA838321 <rodneyakong@gmail.com>
2022-11-18 16:44:12 +0000

    hexa decimal numbers printer in lowerewse

---
+++ alphabet_manipulation/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+//Write a program that prints all the numbers of base 16 in lowercase, followed by a new line.
+//
+//You can only use the putchar function (every other function (printf, puts, etc…) is forbidden)
+//All your code should be in the main function
+//You can only use putchar three times in your code
+
+int main(void)
+{
+	char i;
+	char j;
+	//numbers in base 16 are counted from a-f after 9 i.e 10 is not included.
+	//the ascii values for 0-9 is 48-57.
+	for (i = 48; l < 58; i++)
+	{
+		putchar(i);
+	}
+	for (j = 'a'; j <=f; j++)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+}
