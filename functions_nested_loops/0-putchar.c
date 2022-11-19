0183ae33c1c1506ec720f699ab3befd588d142c6
RNA838321 <rodneyakong@gmail.com>
2022-11-19 20:13:21 +0000

    program to print putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * program that prints _putchar
+ * Author: Akong Rodney
+ * Date: 11/19/2022
+ * Time: 9:05 pm
+ */
+int main()
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+}
