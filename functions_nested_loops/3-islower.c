24c414fb343eebff30634f8ca60b74bc908c2233
RNA838321 <rodneyakong@gmail.com>
2022-11-20 07:36:32 +0000

    program to check for small letters

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * program to check for small letters
+ * character checked @c
+ * return 1 if the character checked is c,else return o
+ * Author: Akong< Rodney 
+ * Date: 11/20/2022
+ * Time: 8:30;
+ */
+int _islower(int c);
+{
+	int v;
+
+	(c >= 'a' && c <= 'z');
+
+	v = c >= 'a' c <= 'z';
+	return v;
+}
