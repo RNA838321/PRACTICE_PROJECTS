bdca6c583097c8a05417a6c95b3b5af7e23f2626
RNA838321 <rodneyakong@gmail.com>
2022-11-21 06:52:41 -0800

    return type function

---
+++ functions_nested_loops/function_returntype.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * function that accepts two values from a 
+ * user and print the sum, the return the value
+ * Author:Akong Rodney
+ * Date:11/21/2022
+ * Time:3:37 pm
+ */
+int sum(int,int);
+int main(void)
+{
+	int m,n.h;
+
+	printf("enterr value of two integer");
+
+	scanf("%d%d", &m ,&n);
+
+	h = sum(m,n);
+
+	printf("%d",h);
+}
+int sum(int k, int l)
+{
+	int v = 0;
+
+	v = k + l;
+
+	return (v);
+}
