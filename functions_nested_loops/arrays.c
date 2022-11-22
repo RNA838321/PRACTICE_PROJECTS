b41f06e6be4c3f3f302f6c70226cd277adf75d11
RNA838321 <rodneyakong@gmail.com>
2022-11-21 19:38:14 -0800

    intro to arrays

---
+++ functions_nested_loops/arrays.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * Intro to arrays and initializations
+ * Author: Akong Rodney
+ * Date: 22/11/2022
+ */
+void main(void)
+{	int a[7],i;
+	printf("please enter the values to be stored in the array:\n");
+
+	for (i = 0; i < 5; i++) 
+	scanf("%d",&a[i]);
+
+	for ("i = 0; i < 5; i++)
+	printf("%d\n",a[i]);
+}
